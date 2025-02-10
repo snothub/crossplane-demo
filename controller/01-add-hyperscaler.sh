c54b0a0161dc3c7ec6e9c97ecd8b1135f7d470bd
Danijel Simeunovic <danijel.simeunovic@trumf.no>
2025-02-10 08:45:48 +0100

    azure alias

--- controller/01-add-hyperscaler.sh
+++ controller/01-add-hyperscaler.sh
@@ -1,16 +1,13 @@
-HYPERSCALER="azure"
-
-echo "export HYPERSCALER=$HYPERSCALER" >> .env
-
 AZURE_TENANT_ID=$(gum input --placeholder "Azure Tenant ID" --value "$AZURE_TENANT_ID")
+AZURE_TENANT_NAME=$(gum input --placeholder "Provide a short alias for tenant" --value "$AZURE_TENANT_NAME")
 
 az login --tenant $AZURE_TENANT_ID
 
 export SUBSCRIPTION_ID=$(az account show --query id -o tsv)
 
-az ad sp create-for-rbac --sdk-auth --role Owner --scopes /subscriptions/$SUBSCRIPTION_ID | tee azure-creds.json
+az ad sp create-for-rbac --sdk-auth --role Owner --scopes /subscriptions/$SUBSCRIPTION_ID | tee azure-creds-$AZURE_TENANT_NAME.json
 
-kubectl --namespace crossplane-system create secret generic azure-creds --from-file creds=./azure-creds.json
+kubectl --namespace crossplane-system create secret generic azure-creds-$AZURE_TENANT_NAME --from-file creds=./azure-creds-$AZURE_TENANT_NAME.json
 
 kubectl apply --filename providers/azure-config.yaml
 
