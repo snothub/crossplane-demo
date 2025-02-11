5799f3d5ecb5d85ba5b3bacca95bf669d3a8179d
Danijel Simeunovic <danijel.simeunovic@trumf.no>
2025-02-11 16:03:58 +0100

    simpl

--- controller/00-intro.sh
+++ controller/00-intro.sh
@@ -6,36 +6,6 @@ gum style \
 	--margin "1 2" --padding "2 4" \
 	'Setup for the Introduction chapter'
 
-gum confirm '
-Are you ready to start?
-Select "Yes" only if you did NOT follow the story from the start (if you jumped straight into this chapter).
-Feel free to say "No" and inspect the script if you prefer setting up resources manually.
-' || exit 0
-
-echo "
-## You will need following tools installed:
-|Name            |Required             |More info                                          |
-|----------------|---------------------|---------------------------------------------------|
-|Linux Shell     |Yes                  |Use WSL if you are running Windows                 |
-|Docker          |Yes                  |'https://docs.docker.com/engine/install'           |
-|kind CLI        |Yes                  |'https://kind.sigs.k8s.io/docs/user/quick-start/#installation'|
-|kubectl CLI     |Yes                  |'https://kubernetes.io/docs/tasks/tools/#kubectl'  |
-|crossplane CLI  |Yes                  |'https://docs.crossplane.io/latest/cli'            |
-|yq CLI          |Yes                  |'https://github.com/mikefarah/yq#install'          |
-|Google Cloud account with admin permissions|If using Google Cloud|'https://cloud.google.com'|
-|Google Cloud CLI|If using Google Cloud|'https://cloud.google.com/sdk/docs/install'        |
-|AWS account with admin permissions|If using AWS|'https://aws.amazon.com'                  |
-|AWS CLI         |If using AWS         |'https://docs.aws.amazon.com/cli/latest/userguide/getting-started-install.html'|
-|Azure account with admin permissions|If using Azure|'https://azure.microsoft.com'         |
-|az CLI          |If using Azure       |'https://learn.microsoft.com/cli/azure/install-azure-cli'|
-
-If you are running this script from **Nix shell**, most of the requirements are already set with the exception of **Docker** and the **hyperscaler account**.
-" | gum format
-
-gum confirm "
-Do you have those tools installed?
-" || exit 0
-
 rm -f .env
 
 #########################
@@ -55,17 +25,6 @@ helm upgrade --install crossplane crossplane \
     --repo https://charts.crossplane.io/stable \
     --namespace crossplane-system --create-namespace --wait
 
-# kubectl apply \
-#     --filename providers/provider-kubernetes-incluster.yaml
-
-# kubectl apply --filename providers/provider-helm-incluster.yaml
-
-# kubectl apply --filename providers/dot-kubernetes.yaml
-
-# kubectl apply --filename providers/dot-sql.yaml
-
-# kubectl apply --filename providers/dot-app.yaml
-
 kubectl apply --filename providers/azure-providers.yaml
 
 gum spin --spinner dot \
@@ -82,12 +41,6 @@ kubectl apply -f https://github.com/bitnami-labs/sealed-secrets/releases/downloa
 ###########
 # Argo CD #
 ###########
-
-REPO_URL=$(git config --get remote.origin.url)
-# workaround to avoid setting up SSH key in ArgoCD
-REPO_URL=$(echo $REPO_URL | sed 's/git@github.com:/https:\/\/github.com\//') # replace git@github.com: to https://github.com/
-
-yq --inplace ".spec.source.repoURL = \"$REPO_URL\"" argocd/apps.yaml
 
 helm upgrade --install argocd argo-cd \
     --repo https://argoproj.github.io/argo-helm \
