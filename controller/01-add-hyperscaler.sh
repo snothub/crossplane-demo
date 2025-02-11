60de65360abfe97b1e1d961311d957d8931ff8de
Danijel Simeunovic <danijel.simeunovic@trumf.no>
2025-02-11 16:12:32 +0100

    fix

--- controller/01-add-hyperscaler.sh
+++ controller/01-add-hyperscaler.sh
@@ -8,7 +8,7 @@ export SUBSCRIPTION_ID=$(az account show --query id -o tsv)
 az ad sp create-for-rbac --sdk-auth --role Owner --scopes /subscriptions/$SUBSCRIPTION_ID | tee azure-creds-$AZURE_TENANT_NAME.json
 
 kubectl --namespace crossplane-system create secret generic azure-creds-$AZURE_TENANT_NAME --dry-run=client --from-file creds=./azure-creds-$AZURE_TENANT_NAME.json -o json | kubeseal | tee sealedsecrets/azure-sealedcreds-$AZURE_TENANT_NAME.yaml
-kubectl apply -f sealedsecrets/*
+kubectl apply -f sealedsecrets/
 rm azure-creds-$AZURE_TENANT_NAME.json
 
 helm upgrade --install --set config.name=$AZURE_TENANT_NAME pr-config ./providerconfig 
